d72b3278a8cffce73fdb17db8de087cc12bc7c57
Ahmed Fayyaz <wwwizhr@gmail.com>
2026-02-22 00:58:05 +0500

    correct logic for counter using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 function App() {
   let [counter, setCounter] = useState(0)
- const addValue = (counter) => {
+  const addValue = () => {
     setCounter(counter + 1)
     console.log('Value Added', counter)
   }
-  const removeValue = (counter) => {
+  const removeValue = () => {
+    if(counter === 0) return
     setCounter(counter - 1)
     console.log('Value Subtracted', counter)
   }
